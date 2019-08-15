346f844d04faef8b45d2f18d916197c2e7994d22
Tomasz Bujnowicz <tomasz@prollective.com>
2019-08-15 22:32:53 +0200

    Update 2019-08-15T20:32:51.131Z

--- gulpfile.babel.js/util/env.js
+++
@@ -1,7 +0,0 @@
-/*
- * @title Environments
- */
-
-export const NODE_ENV = process.env.NODE_ENV ? "production" : "development";
-export const isDev = process.NODE_ENV === "development";
-export const isProd = process.env.NODE_ENV === 'production';
